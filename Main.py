554a3ce8d8268511cf05da2d874c75da1dff7fa4
Pablo Berjon <2490772B@student.gla.ac.uk>
2020-06-06 20:58:25 +0100

    Some small changes to number of nodes and added epochs for the training

--- Main.py
+++ Main.py
@@ -3,55 +3,65 @@ from neuralNetwork import neuralNetwork
 
 # nodes and learning rate
 input_layer = 784
-hidden_layer = 100
+hidden_layer = 100 # Optimal is 200, but it takes considerably longer to train and the % difference is about 1%
 output_layer = 10
-learning_rate = 0.3
+learning_rate = 0.1
 
 # Instance of neural network
 nn = neuralNetwork(input_layer, hidden_layer, output_layer, learning_rate)
 
 # Loading training dataset - this code is for the small sample
-training_data_file = open("mnist_dataset/mnist_train_100.csv", 'r')
-training_data_list = training_data_file.readlines()
-training_data_file.close()
-
-# Loading training dataset - this code is for the big sample. You need to unzip the full data sets
-# training_data_file = open("mnist_dataset\mnist_full_dataset\mnist_train.csv", 'r')
+# training_data_file = open("mnist_dataset/mnist_train_100.csv", 'r')
 # training_data_list = training_data_file.readlines()
 # training_data_file.close()
 
-# Neural Network training - iteration through all data sets
+# Loading training dataset - this code is for the big sample. You need to unzip the full data sets
+training_data_file = open("mnist_dataset\mnist_full_dataset\mnist_train.csv", 'r')
+training_data_list = training_data_file.readlines()
+training_data_file.close()
 
-for entry in training_data_list:
+# TRAINING - Epochs are the naming standard for iterative trainings
 
-    all_values = entry.split(',')
+#   We set the epoch = 2; which means that we will perform two training
+#   sessions with the same training list of values, which means that the
+#   training time will double.
 
-    # For the NN to work, we need the values to be in the range from 0.01 to 0.99
+epochs = 5
 
-    scaled_inputs = (numpy.asfarray(all_values[1:]) / 255.0*0.99) + 0.01
+for epoch in range(epochs):
 
-    # The targeted values: they start at the beginning of each data set (for example,
-    # the first number in the data set is 5, therefore the values afterward correspond
-    # to the number 5.
-    targets = numpy.zeros(output_layer) + 0.01
+    # Neural Network training - iteration through all data sets
+    for entry in training_data_list:
 
-    # the target value we mentioned before. it will allocate the number 0.99 in the array
-    targets[int(all_values[0])] = 0.99
-    nn.train(scaled_inputs, targets)
-    pass
+        all_values = entry.split(',')
+
+        # For the NN to work, we need the values to be in the range from 0.01 to 0.99
+
+        scaled_inputs = (numpy.asfarray(all_values[1:]) / 255.0*0.99) + 0.01
+
+        # The targeted values: they start at the beginning of each data set (for example,
+        # the first number in the data set is 5, therefore the values afterward correspond
+        # to the number 5.
+        targets = numpy.zeros(output_layer) + 0.01
+
+        # the target value we mentioned before. it will allocate the number 0.99 in the array
+        targets[int(all_values[0])] = 0.99
+        nn.train(scaled_inputs, targets)
+
+pass
 
 # Neural Network testing - same as before, but we expect the correct result as output
 # from the NN. This time, we load the test data for the test process
 
 # This code is for the small data set sample
-test_data_file = open("mnist_dataset/mnist_test_10.csv", 'r')
-test_data_list = test_data_file.readlines()
-test_data_file.close()
-
-# This code is for the big data set sample. You need to unzip the full data sets
-# test_data_file = open("mnist_dataset\mnist_full_dataset\mnist_test.csv", 'r')
+# test_data_file = open("mnist_dataset/mnist_test_10.csv", 'r')
 # test_data_list = test_data_file.readlines()
 # test_data_file.close()
+
+# This code is for the big data set sample. You need to unzip the full data sets
+test_data_file = open("mnist_dataset\mnist_full_dataset\mnist_test.csv", 'r')
+test_data_list = test_data_file.readlines()
+test_data_file.close()
 
 # Score card gives points to check how well the NN has performed
 score_card = []
